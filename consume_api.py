5bcefde064a3c5aa922b1962ff635437b9c4b3ea
Daniel Kiss <danielkiss@Daniels-MacBook-Pro.local>
2023-03-17 11:43:52 -0700

    add logic for what data to show

--- consume_api.py
+++ consume_api.py
@@ -4,5 +4,15 @@ import json
 response = requests.get(
     'http://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow')
 
-print(response.json())
-print(type(response))
+# to see the data type of the response and convert it into an object python can work with see below
+# print(type(response))
+# print(response.json()['items'])
+
+for data in response.json()['items']:
+    if data['is_answered']:
+        # below we are grabbing the title attribute
+        print(data['title'])
+        print(data['link'])
+    else:
+        print("skipped")
+    print("---")
